25d4d38db4b10c182acfe8f09fd925a38faccad5
dking3876 <dking3876@msn.com>
2016-01-29 10:07:34 -0500

    remove double dashes in slug

--- BraftonLibrary/BraftonLibrary.php
+++ BraftonLibrary/BraftonLibrary.php
@@ -221,7 +221,7 @@ function create_topic($topic,$existing_topics){
     $url = 'https://api.hubapi.com/blogs/v3/topics?hapikey=' . hub_apiKey . '&casing=snake_case';
     $params = array(
         'name' => $topic,
-         'slug'  => strtolower(preg_replace('/[^A-Za-z0-9\-]/','',str_replace(' ','-',$topic))),
+         'slug'  => str_replace('--','-',strtolower(preg_replace('/[^A-Za-z0-9\-]/','',str_replace(' ','-',$topic)))),
     );
     if(DEBUG){
         $check_params = $params;
