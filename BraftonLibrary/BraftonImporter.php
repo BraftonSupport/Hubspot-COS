1186bb873c359f3aac657feecb949d5f2fc809f4
unknown <edwardhornig@gmail.com>
2016-05-16 11:34:58 -0400

    change slug generation on videos to match articles

--- BraftonLibrary/BraftonImporter.php
+++ BraftonLibrary/BraftonImporter.php
@@ -25,10 +25,10 @@ class BraftonImporter {
         $articles_imported = 0;
 
         foreach ($articles as $a) {
-                        
+
             $strPost = '';
             $createCat = array();
-            $brafton_id = $a->getId();      
+            $brafton_id = $a->getId();
             $post_title = $a->getHeadline();
             echo "Checking: ".$post_title."<br/>";
             // check against existing posts here.  Use title.
@@ -56,12 +56,12 @@ class BraftonImporter {
             }
 
 			$article_topics = array();
-		
+
 			foreach($createCat as $brafton_cat){
 
                 $topic_exists = false;
 				$new_cat = false;
-                foreach ($existing_topics as $topic) { 
+                foreach ($existing_topics as $topic) {
                     if(strtolower($brafton_cat) == strtolower($topic)){
 			            $article_topics[] = array_search( $topic, $existing_topics);
                         $topic_exists = true;
@@ -69,7 +69,7 @@ class BraftonImporter {
                     }
 
                 }
-				
+
 				if(!$topic_exists){
 					$c = create_topic($brafton_cat,$existing_topics);
 					$response = $c[0];
@@ -77,15 +77,15 @@ class BraftonImporter {
 					$article_topics[] = $response->id;
 				}
 			}
-                
-                $photos = $a->getPhotos();  
+
+                $photos = $a->getPhotos();
                 if(!empty($photos)){
                     $image = $photos[0]->getLarge();
                     $post_image = $image->getUrl();
                 }
                 $post_summary = $post_excerpt;
                 if(!empty($post_image)){
-                    $image_id = $photos[0]->getId();                
+                    $image_id = $photos[0]->getId();
                     $image_small = $photos[0]->getThumb();
                     $post_image_caption = "<div style='padding:6px;'>" . $photos[0]->getCaption() . "</div>";
 
@@ -95,11 +95,11 @@ class BraftonImporter {
                     }
 
                     $divcode = '<div style="width:300px;background-color: #F9F9F9; border-radius:10px; padding: 3px;font: 11px/1.4em Arial, sans-serif;margin: 0.5em 0.8em 0.5em 0.5em; float:right;">';
-                    $imgcode = '<img src="' . $post_image . '" style = "width:300px;vertical-align:middle; margin-bottom: 3px;"  alt="'.$photos[0]->getCaption().'" />'; 
-                    $excerptImage = '<img src="' . $post_image . '" style = "width:300px;height:auto;vertical-align:middle; margin-bottom: 3px;float:right"  alt="'.$photos[0]->getCaption().'" />'; 
+                    $imgcode = '<img src="' . $post_image . '" style = "width:300px;vertical-align:middle; margin-bottom: 3px;"  alt="'.$photos[0]->getCaption().'" />';
+                    $excerptImage = '<img src="' . $post_image . '" style = "width:300px;height:auto;vertical-align:middle; margin-bottom: 3px;float:right"  alt="'.$photos[0]->getCaption().'" />';
                     $div = $divcode . $imgcode . '<br/>' . $post_image_caption . '</div>';
                     $strPost = $strPost.$div.$post_content;
-                    $post_summary =  $excerptImage . $post_excerpt; 
+                    $post_summary =  $excerptImage . $post_excerpt;
                 } else {
                     $strPost= $strPost . $post_content;
                 }
@@ -109,7 +109,7 @@ class BraftonImporter {
             else{
                 $author = author_id;
             }
-            
+
             echo '<br/>This is the author : '.$author.'<br/>';
             $post = new brafton_post($post_title,$post_excerpt,$slug,$strPost,$post_summary,$author,$article_topics,false,$post_date, false, $post_image);
 
@@ -119,7 +119,7 @@ class BraftonImporter {
                 );
             /*
             if(imgage_import){
-                $response = $post->update_post($id, $featuredImage);   
+                $response = $post->update_post($id, $featuredImage);
                 if(DEBUG){
                     echo '<pre>';
                     var_dump($response);
@@ -128,13 +128,13 @@ class BraftonImporter {
             }
             */
             if(post_status == 'published'){
-                $post->publish_post($id);  
+                $post->publish_post($id);
             }
             // broken...  what is time format of date on brafton feed?  will almost certainly need to convert.
             $articles_imported++;
         }
     }
-    
+
     /**********************************************************************************************
  *
  * Import Videos from the XML Feeds
@@ -144,7 +144,7 @@ class BraftonImporter {
     function import_videos($titles,$existing_topics){
         echo " Videos: <br/>";
         $params = array('max'=>99);
-        
+
         $baseURL = 'http://livevideo.'.str_replace('http://', '',domain).'/v2/';
         echo $baseURL;
         $videoClient = new AdferoVideoClient($baseURL, brafton_video_publicKey, brafton_video_secretKey);
@@ -166,12 +166,12 @@ class BraftonImporter {
         $articles = $articleClient->ListForFeed($feedList->items[0]->id,'live',0,100);
 
         $articles_imported = 0;
-        
+
         //fix for video feed issue present on june 24 2015.  video feeds are supposed to be sorted by modified date in desc order but are showing up in asc order instead. //remove once fixed. note dk
         $articles->items = array_reverse($articles->items);
-        
+
        foreach ($articles->items as $a) {
-            
+
             //max of five articles imported
 
             $thisArticle = $client->Articles()->Get($a->id);
@@ -182,7 +182,7 @@ class BraftonImporter {
             $post_title = $thisArticle->fields['title'];
 
             $post_date = $thisArticle->fields['lastModifiedDate'];
-            
+
             $post_content = $thisArticle->fields['content'];
 
             $post_excerpt = $thisArticle->fields['extract'];
@@ -192,27 +192,18 @@ class BraftonImporter {
             // check against existing posts here.  Use title.
             echo "Checking: ".$post_title."<br/>";
             if (compare_post_titles($post_title,$titles)) continue;
-           
+
             $articles_imported++;
             if($articles_imported>5) break;
-           
-            echo "POSTING: ".$post_title."<br>";
-            
-            $words = str_word_count($post_title,1);
 
-            $count = 0;
-            $slug='';
-            foreach ($words as $word){
-                $count++;
-                $slug .= $word;
-                if($count>6) break;
-                $slug .= '_';
-            }
+            echo "POSTING: ".$post_title."<br>";
+
+            $slug=str_replace(' ','-',$post_title);
             //$meta = $a->getHtmlMetaDescription();
-            
+
             // Enter Author Tag
             $author = author_id;
-			
+
 			$categories = $client->Categories();
 			if(isset($categories->ListForArticle($a->id,0,100)->items[0]->id)){
 				$categoryId = $categories->ListForArticle($a->id,0,100)->items[0]->id;
@@ -222,16 +213,16 @@ class BraftonImporter {
 			}
 
 			$article_topics = array();
-		
+
 			foreach($createCat as $brafton_cat){
 				echo "topic first loop: $brafton_cat <br/>";
                 $topic_exists = false;
 				$new_cat = false;
-                foreach ($existing_topics as $topic) { 
+                foreach ($existing_topics as $topic) {
 				echo "topic second loop: $topic <br/>";
                     if(strtolower($brafton_cat) == strtolower($topic)){
 			echo "topic exists <br/>";
-			echo "brafton cat: ". $brafton_cat . '<br/>';  
+			echo "brafton cat: ". $brafton_cat . '<br/>';
                         echo "topic: $topic <br/>";
 			$article_topics[] = array_search( $topic, $existing_topics);
                         $topic_exists = true;
@@ -239,7 +230,7 @@ class BraftonImporter {
                     }
 
                 }
-				
+
 				if(!$topic_exists){
 					echo "creating topic: $brafton_cat <br/>";
 					$c = create_topic($brafton_cat,$existing_topics);
@@ -248,7 +239,7 @@ class BraftonImporter {
 					$article_topics[] = $response->id;
 				}
 			}
-           
+
             $thisPhoto = $photos->ListForArticle($brafton_id,0,100);
             if(isset($thisPhoto->items[0]->id)){
                     $photoId = $photos->Get($thisPhoto->items[0]->id)->sourcePhotoId;
@@ -260,27 +251,27 @@ class BraftonImporter {
                     $post_image_caption = $photos->Get($thisPhoto->items[0]->id)->fields['caption'];
                                 echo 'Photo caption : '.$post_image_caption.'<br/>';
                     $image_alt = '';
-                    
+
                     $image_id = $thisPhoto->items[0]->id;
                     if(image_import){
                         $post_image = upload_image($post_image);
                     }
                 $excerptImage = '<img src="' . $post_image . '" style = "width:300px;height:auto;vertical-align:middle; margin-bottom: 3px;float:right"  alt="Google Logo" />';
-                $post_excerpt =  $excerptImage . $post_excerpt; 
+                $post_excerpt =  $excerptImage . $post_excerpt;
             }
-           
-                // $photos = $a->getPhotos();  
-            
+
+                // $photos = $a->getPhotos();
+
                 // $image = $photos[0]->getLarge();
-                
+
                 // $post_image = $image->getUrl();
-                
+
                 // if(!empty($post_image)){
 
-                //     $image_id = $photos[0]->getId();                
+                //     $image_id = $photos[0]->getId();
 
                 //     $image_small = $photos[0]->getThumb();
-                    
+
                 //     $post_image_small = $image_small->getURL();
 
                 //     $post_excerpt = $post_excerpt.'<img src = "'.$post_image.'" alt ="" /><p>'.$post_content.'</p>' ;
@@ -288,25 +279,25 @@ class BraftonImporter {
 
                 $presplash = $thisArticle->fields['preSplash'];
                 $postsplash = $thisArticle->fields['postSplash'];
-                                
+
                 $videoList=$videoOutClient->ListForArticle($brafton_id,0,10);
                 $list=$videoList->items;
 
                 if (video_player == "atlantisjs")
-                    $embedCode = sprintf( "<video id='video-%s' class=\"ajs-default-skin atlantis-js\" controls preload=\"auto\" width='512' height='288' poster='%s' >", $brafton_id, $presplash ); 
-                
+                    $embedCode = sprintf( "<video id='video-%s' class=\"ajs-default-skin atlantis-js\" controls preload=\"auto\" width='512' height='288' poster='%s' >", $brafton_id, $presplash );
+
                 else
-                    $embedCode = sprintf( "<video id='video-%s' class='video-js vjs-default-skin' controls preload='auto' width='512' height='288' poster='%s' data-setup src='%s' >", $brafton_id, $presplash, $path ); 
+                    $embedCode = sprintf( "<video id='video-%s' class='video-js vjs-default-skin' controls preload='auto' width='512' height='288' poster='%s' data-setup src='%s' >", $brafton_id, $presplash, $path );
 
                 foreach($list as $listItem){
                     $output=$videoOutClient->Get($listItem->id);
                     //logMsg($output->path);
                     $type = $output->type;
-                    $path = $output->path; 
-                    $resolution = $output->height; 
+                    $path = $output->path;
+                    $resolution = $output->height;
                     $source = generate_source_tag( $path, $resolution );
-                    $embedCode .= $source; 
-                }       
+                    $embedCode .= $source;
+                }
                 $embedCode .= '</video>';
                 //old code
                 //$embedCode = $videoClient->VideoPlayers()->GetWithFallback($brafton_id, 'redbean', 1, 'rcflashplayer', 1);
@@ -320,7 +311,7 @@ class BraftonImporter {
                     $script .= '}]';
                     $script .= '});';
                     $script .=  '</script>';
-                    $ctascript = $script; 
+                    $ctascript = $script;
                     */
                                 $ctas = '';
             $pause_text = video_pause_text;
@@ -367,8 +358,8 @@ EOC;
             /*
 
             create/publish posts
-            tbd: topics (categories), not hotlinking images?            
-        
+            tbd: topics (categories), not hotlinking images?
+
             */
 
 
@@ -378,11 +369,11 @@ EOC;
             $id = $post->article_id;
 
             if(post_status == 'published'){
-                $post->publish_post($id);  
+                $post->publish_post($id);
             }
 
         }
-        
+
     }
 }
 ?>
